7387a00b7801e096cf73a72ff442e26cf7174271
Matéo <129748653+MatKim76@users.noreply.github.com>
2024-11-20 13:54:45 +0100

    Create Q5.java

---
+++ Q5.java
@@ -0,0 +1,30 @@
+public class Q5 {
+    public static void main(String[] args) 
+  {
+        if (args.length != 1) 
+        {
+            System.out.println("Mettre un nombre d'étoile");
+            return;
+        }
+
+        try {
+            int nombreEtoiles = Integer.parseInt(args[0]);
+
+            if (nombreEtoiles < 0) {
+                System.out.println("Veuillez entrer un nombre positif.");
+                return;
+            }
+
+            for (int i = 1; i <= nombreEtoiles; i++) 
+            {
+                for (int j = i; j > 0; j--)
+                  {
+                    System.out.print("*");
+                }
+                System.out.println(); 
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Veuillez entrer un nombre entier valide.");
+        }
+    }
+}
